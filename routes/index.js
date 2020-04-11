bee6b4698287db22a9cc52badd8858ef844e0a77
Deepak Singh <deepak.negi343@gmail.com>
2020-04-11 13:52:32 +0530

    changes in index.js of routes

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,4 @@ const express=require('express');
 const router=express.Router();
 router.use('/doctors',require('./doctors'));
 router.use('/patients',require('./patients'));
-// router.use('/reports',require('./reports'));
 module.exports=router;
